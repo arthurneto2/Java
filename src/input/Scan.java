9532e77ee1ab53c5c2bcbfdcf7509d336a031db6
ArthurNeto <arthurneto779@gmail.com>
2025-09-04 14:15:30 -0300

    add Scanner class for user input

---
+++ src/input/Scan.java
@@ -0,0 +1,23 @@
+package input;
+
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Digite a sua Idade: ");
+        int idade = Integer.parseInt(scan.nextLine());
+
+        System.out.println("Digite a sua altura: ");
+        double altura = Double.parseDouble(scan.nextLine());
+
+        System.out.println("\nolá " + nome + "...");
+        System.out.println("sua idade é: " + idade);
+        System.out.println("sua altura é: " + altura);
+    }
+}
